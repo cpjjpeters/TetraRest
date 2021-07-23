d79a1623980d8d1d0945df2728292ed215042c15
cpjjpeters <cpjjpeters@gmail.com>
2021-07-23 12:17:13 +0200

    prepare the TopicService

--- src/test/java/com/example/tetra/TetraRestDemo/TetraRestDemoApplicationTests.java
+++ src/test/java/com/example/tetra/TetraRestDemo/TetraRestDemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.tetra.TetraRestDemo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.tetra.TetraRestDemo.model.Topic;
+import com.example.tetra.TetraRestDemo.service.TopicService;
 
 @SpringBootTest
 class TetraRestDemoApplicationTests {
-
+	@Autowired
+	TopicService topicService;
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void testGetAllTopics() {
+		List<Topic> topics = topicService.getAllTopics();
+		assertEquals(2, topics.size());
+	}
 }
